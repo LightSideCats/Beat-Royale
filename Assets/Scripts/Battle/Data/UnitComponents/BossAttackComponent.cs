133afce8b0e4781d77a3906e202c942dcb4d1f8a
Malvis Light <cat@lightside.games>
2024-10-24 13:48:37 +0400

    LSCore: Improved BattleModule❤️

--- Assets/Scripts/Battle/Data/UnitComponents/BossAttackComponent.cs
+++ Assets/Scripts/Battle/Data/UnitComponents/BossAttackComponent.cs
@@ -13,8 +13,30 @@ namespace Battle.Data.UnitComponents
     [Serializable]
     public class BossAttackComponent : BaseComp
     {
+        [Serializable]
+        public abstract class AttackSelector
+        {
+            public BaseAttack Select(List<BaseAttack> attacks)
+            {
+                return Select(attacks.Where(x => x.CanAttack));
+            }
+            
+            protected abstract BaseAttack Select(IEnumerable<BaseAttack> attacks);
+        }
+
+        [Serializable]
+        public class RandomAttackSelector : AttackSelector
+        {
+            protected override BaseAttack Select(IEnumerable<BaseAttack> attacks)
+            {
+                var attack = attacks.RandomElement();
+                return attack;
+            }
+        }
+        
         [SerializeField] protected FindTargetFactory findTargetFactory;
         [SerializeReference] protected List<BaseAttack> attacks;
+        [SerializeReference] protected AttackSelector attackSelector;
         
         protected FindTargetComp findTargetComp;
         public float cooldown = 0.5f;
@@ -33,18 +55,36 @@ namespace Battle.Data.UnitComponents
 
         private void Attack()
         {
-            var attack = attacks.Where(x => x.CanAttack).RandomElement();
+            var attack = attackSelector.Select(attacks);
             if (attack != null)
             {
-                DOTween.Sequence()
-                    .Append(attack.Attack())
-                    .AppendInterval(cooldown)
-                    .OnComplete(Attack);
+                var (attackTween, cooldownTween) = attack.Trigger(cooldown);
+
+                if (attackTween != null)
+                {
+                    var sequence = DOTween.Sequence()
+                        .Append(attackTween)
+                        .OnComplete(Attack);
+
+                    if (cooldownTween != null)
+                    {
+                        sequence.Append(cooldownTween);
+                    }
+                }
+                else
+                {
+                    goto waitFrame;
+                }
             }
             else
             {
-                Wait.Frames(1, Attack);
+                goto waitFrame;
             }
+            
+            return;
+            
+            waitFrame:
+            Wait.Frames(1, Attack);
         }
     }
 }
